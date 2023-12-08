9fe6fec4ff4fb243f6dbbe9f4482a1e3ee7eee24
basem saeed seed <88434009+basem19@users.noreply.github.com>
2023-12-08 04:49:55 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,10 +1,14 @@
 // Start Settings
 let firstName = document.getElementById('first')
+let lastName = document.getElementById('last')
 let email = document.getElementById('email')
 
 firstName.oninput = show
 
+
 function show() {
     email.value = this.value + "@gmail.com"
 }
+
 // End Settings
+
